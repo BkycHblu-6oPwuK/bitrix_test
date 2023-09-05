621813c65a578ae0ea46978e9611423b53f4db0b
BkycHblu-6oPwuK <sanya_belyy_0202@mail.ru>
2023-09-05 17:48:00 +0000

    first commit

--- test.php
+++ test.php
@@ -2,5 +2,6 @@
 require($_SERVER['DOCUMENT_ROOT'].'/local/templates/Test/header.php');
 $APPLICATION->SetTitle("Главная");
 ?><?
+123;
 require($_SERVER['DOCUMENT_ROOT'].'/local/templates/Test/footer.php');
 ?>
